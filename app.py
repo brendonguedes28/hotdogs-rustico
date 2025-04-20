03c44c9c0d6b40645f18228d3d0849e003eee8a9
brendonguedes28 <brendon145@hotmail.com>
2025-04-20 00:50:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -333,10 +333,6 @@ def delete_item(item_id):
     flash('Item excluído com sucesso!', 'success')
     return redirect(url_for('admin'))
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello, World!"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Usa a variável de ambiente PORT, ou 5000 por padrão
+    app.run(host="0.0.0.0", port=port)  # Especifica que o Flask deve rodar na porta correta
