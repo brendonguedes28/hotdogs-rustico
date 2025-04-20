23d27b6a86332c1986f75bf2c793fe1082023f6f
brendonguedes28 <brendon145@hotmail.com>
2025-04-19 23:05:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.secret_key = os.environ.get("SESSION_SECRET", "dev_secret_key")
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)  # needed for url_for to generate with https
 
 # Configure PostgreSQL database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@hostname:port/database_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "pool_recycle": 300,
     "pool_pre_ping": True,
